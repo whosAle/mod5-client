e277bdb5339615f7dbceac4954223150fecc3bb8
whosAle <abelgrave95@gmail.com>
2019-06-19 15:23:01 -0400

    spacing of completion filter to right

--- src/components/ProjectFilter.js
+++ src/components/ProjectFilter.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'flex-end',
+  },
+  formControl: {
+    marginRight: theme.spacing(3),
+    width: "20%",
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 const ProjectFilter = (props) => {
+  const classes = useStyles();
+
+  const [filterData, setFilterData] = useState({completion: "", sort: ""});
+
   const { project } = props;
   console.log("pROJECT:", props);
 
@@ -10,33 +36,30 @@ const ProjectFilter = (props) => {
 
   const handleCompletionFilter = (event) =>{
     debugger;
+    setFilterData({...filterData, [event.target.name]: event.target.value});
     props.onFilterClick(event.target.value);
   }
 
   return (
     <div className="" id="">
       <h1>PROJECT FILTER</h1>
-      <form className="" onSubmit={handleSubmit}>
+      <form form className={classes.root} autoComplete="off" onSubmit={handleSubmit}>
 
-        <label name=""/> Filter By:
-        <select name="">
-          <option>All</option>
-          <option>Capital H-L</option>
-          <option>Capital L-H</option>
-          <option>Name</option>
-          <option>Date</option>
-        </select>
-
-        <label name="completion"/> Completion Filter:
-        <select name="filter" onChange={handleCompletionFilter}>
-          <option>All</option>
-          <option>completed</option>
-          <option>in progress</option>
-          <option>available</option>
-        </select>
-
-        <br/>
-        <input type="submit"/>
+        <FormControl className={classes.formControl}>
+          <Select
+            value={filterData.completion}
+            onChange={handleCompletionFilter}
+            displayEmpty
+            name="completion"
+            className={classes.selectEmpty}
+          >
+            <MenuItem value="all">All</MenuItem>
+            <MenuItem value="completed">Completed</MenuItem>
+            <MenuItem value="in progress">In Progress</MenuItem>
+            <MenuItem value="available">Available</MenuItem>
+          </Select>
+          <FormHelperText>Filter By Completion</FormHelperText>
+        </FormControl>
       </form>
     </div>
   );
@@ -45,11 +68,22 @@ const ProjectFilter = (props) => {
 export default ProjectFilter
 
 
-// t.integer "user_id"
-// t.integer "base_capital"
-// t.string "location"
-// t.string "title"
-// t.string "description"
-// t.string "category"
-// t.boolean "completed"
-// t.boolean "inprogress"
+// <label name=""/> Filter By:
+// <select name="">
+//   <option>All</option>
+//   <option>Capital H-L</option>
+//   <option>Capital L-H</option>
+//   <option>Name</option>
+//   <option>Date</option>
+// </select>
+//
+// <label name="completion"/> Completion Filter:
+// <select name="filter" onChange={handleCompletionFilter}>
+//   <option>All</option>
+//   <option>completed</option>
+//   <option>in progress</option>
+//   <option>available</option>
+// </select>
+//
+// <br/>
+// <input type="submit"/>
