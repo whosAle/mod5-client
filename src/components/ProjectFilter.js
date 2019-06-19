a4a34a38d9757e6a4863e6a39e9223aa37f4e845
whosAle <abelgrave95@gmail.com>
2019-06-19 16:46:44 -0400

    search bar on explore

--- src/components/ProjectFilter.js
+++ src/components/ProjectFilter.js
@@ -3,20 +3,31 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import SearchIcon from '@material-ui/icons/Search';
+
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
     flexWrap: 'wrap',
-    justifyContent: 'flex-end',
+    justifyContent: 'space-evenly',
+    alignItems: 'flex-end',
     marginRight: theme.spacing(3),
   },
-  formControl: {
+  filterControl: {
     margin: theme.spacing(1),
     width: "20%",
     minWidth: 120,
+  },
+  searchControl: {
+    margin: theme.spacing(1),
+    width: "40%",
+    minWidth: 200,
   },
   selectEmpty: {
     marginTop: theme.spacing(2),
@@ -27,6 +38,7 @@ const ProjectFilter = (props) => {
   const classes = useStyles();
 
   const [filterData, setFilterData] = useState({completion: "", sort: ""});
+  const [searchQuery, setSearchQuery] = useState("");
 
   const { project } = props;
   console.log("pROJECT:", props);
@@ -41,24 +53,44 @@ const ProjectFilter = (props) => {
     props.onFilterClick(event.target.value);
   }
 
+  const handleSearchChange = (event) =>{
+    setSearchQuery(event.target.value);
+    // props.onFilterClick(event.target.value);
+  }
+
   return (
     <div className="" id="">
       <form form className={classes.root} autoComplete="off" onSubmit={handleSubmit}>
 
-        <FormControl className={classes.formControl}>
+        <FormControl className={classes.searchControl}>
+          <InputLabel htmlFor="Search By Name">Search By Name</InputLabel>
+          <Input
+            id="project-search"
+            value={searchQuery}
+            onChange={handleSearchChange}
+            startAdornment={
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+
+        <FormControl className={classes.filterControl}>
+          <InputLabel shrink htmlFor="Filter-By-Completion">Filter By Completion</InputLabel>
           <Select
             value={filterData.completion}
             onChange={handleCompletionFilter}
             displayEmpty
             name="completion"
             className={classes.selectEmpty}
+            input={<Input name="completion" id="Filter-By-Completion" />}
           >
             <MenuItem value="all">All</MenuItem>
             <MenuItem value="completed">Completed</MenuItem>
             <MenuItem value="in progress">In Progress</MenuItem>
             <MenuItem value="available">Available</MenuItem>
           </Select>
-          <FormHelperText>Filter By Completion</FormHelperText>
         </FormControl>
       </form>
     </div>
